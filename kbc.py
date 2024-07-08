27f20b3a14b243a5e9fe60ad103711c189c2dd74
Ronak Dhingra <159307752+Gundaboiii@users.noreply.github.com>
2024-07-07 19:18:32 -0500

    Add files via upload

---
+++ kbc.py
@@ -0,0 +1,22 @@
+a = ['Wh0 are you?','How are you?']
+b = ['Ronak','Good']
+print(a[0])
+print('1. Ronak')
+print('2. Manan')
+print('3. Soham')
+print('4. Sussi')
+answer = int(input("Enter option number: "))
+if answer == 1:
+    print('You have won $100')
+else:
+    print('Sorry you have lost the game!')
+print(a[1])
+print('1. Fine')
+print('2. Good')
+print('3. Amazing')
+print('4. Bad')
+answer = int(input("Enter option number: "))
+if answer == 2:
+    print('You have won $200')
+else:
+    print('Sorry you have lost the game!')
