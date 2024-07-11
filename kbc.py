b5302582c887f8cda78d760a613c425b4246a6ab
Ronak Dhingra <159307752+Gundaboiii@users.noreply.github.com>
2024-07-10 22:50:55 -0500

    Update kbc.py

--- kbc.py
+++ kbc.py
@@ -1,22 +1,15 @@
-a = ['Wh0 are you?','How are you?']
-b = ['Ronak','Good']
-print(a[0])
-print('1. Ronak')
-print('2. Manan')
-print('3. Soham')
-print('4. Sussi')
-answer = int(input("Enter option number: "))
-if answer == 1:
-    print('You have won $100')
-else:
-    print('Sorry you have lost the game!')
-print(a[1])
-print('1. Fine')
-print('2. Good')
-print('3. Amazing')
-print('4. Bad')
-answer = int(input("Enter option number: "))
-if answer == 2:
-    print('You have won $200')
-else:
-    print('Sorry you have lost the game!')
+def questions(option_number):
+    question_list = ['Who are you?','How are you?']
+def answers(option_number):
+    asnwer_list = ['Ronak','Good']
+def check(question_list,answer_list,option_number):
+    if (question_list[option_number]==answer_list[option_number]):
+        print("Your answer is Correct! You have won $")
+    else:
+        pass
+def game(question_list,answer_list,option_number):
+    print("Welcome to the game! You have to answer the questions correctly to win the game!")
+    print("You have 4 options to choose from! Good Luck!")
+    for i in range(2):
+        print(question_list[i])
+    pass
